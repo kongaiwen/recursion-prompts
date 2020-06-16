9b94e187e911d246f471ec1523a7fd49e336d845
Evan Kolb <kongaiwen@gmail.com>
2020-06-16 15:15:35 -0400

    Write replaceKeysInObj function

--- src/recursion.js
+++ src/recursion.js
@@ -480,6 +480,22 @@ var countValuesInObj = function(obj, value) {
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
 // them to a provided new name while preserving the value stored at that key.
 var replaceKeysInObj = function(obj, oldKey, newKey) {
+
+  if (typeof obj !== 'object') {
+    return;
+  }
+
+  for (var key in obj) {
+    if (key === oldKey) {
+      replaceKeysInObj(obj[key], oldKey, newKey);
+      obj[newKey] = obj[oldKey];
+      delete obj[oldKey];
+    } else {
+      replaceKeysInObj(obj[key], oldKey, newKey);
+    }
+  }
+
+  return obj;
 };
 
 // 25. Get the first n Fibonacci numbers. In the Fibonacci sequence, each subsequent
